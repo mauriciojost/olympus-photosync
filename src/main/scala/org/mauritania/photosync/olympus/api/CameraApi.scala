05513efbec237359efd6d3e422817dbeccbf3d9d
Mauricio Jost <mauricio.jost@gmail.com>
2015-08-09 00:41:58 +0200

    Add documentation

--- src/main/scala/org/mauritania/photosync/olympus/api/CameraApi.scala
+++ src/main/scala/org/mauritania/photosync/olympus/api/CameraApi.scala
@@ -12,18 +12,22 @@ import scala.io.{BufferedSource, Source}
 import scala.util.matching.Regex
 
 class CameraApi(
+
            /**
             * Protocol to be used to contact the server.
+            * Should not be changed.
             */
            val serverProtocol: String = "http",
 
            /**
             * Name or IP address of the server.
+            * Should not be changed.
             */
            val serverName: String = "oishare",
 
            /**
             * Port of the http service provided by the server.
+            * Should not be changed.
             */
            val serverPort: Int = 80,
 
@@ -40,12 +44,14 @@ class CameraApi(
 
            /**
             * Timeout (in ms) to be used when pinging the server.
+            * Should not be changed.
             */
            val serverPingTimeout: Int = 2000,
 
            /*
             * Regex used to identify files from the server's response
             * Sample: wlansd[17]="/DCIM/100OLYMP,P7290009.JPG,278023,0,18173,42481";
+            * Should not be changed.
             */
            val fileRegex: Regex = """.*=.*,(\w+\.\w+),(\d+),.*""".r
 
