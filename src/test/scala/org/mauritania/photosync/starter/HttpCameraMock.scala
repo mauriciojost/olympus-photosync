c5f29cd55330da505d600e55ba86bff8f1860f75
Mauricio Jost <mauricio.jost@gmail.com>
2018-01-23 20:47:29 +0100

    Improve HttoCameraMock

--- src/test/scala/org/mauritania/photosync/starter/HttpCameraMock.scala
+++ src/test/scala/org/mauritania/photosync/starter/HttpCameraMock.scala
@@ -1,10 +1,13 @@
 package org.mauritania.photosync.starter
 
 import com.sun.net.httpserver.{HttpExchange, HttpHandler}
+import org.slf4j.LoggerFactory
 
 class HttpCameraMock extends HttpHandler {
 
-  val rootResponse = """
+  val logger = LoggerFactory.getLogger(this.getClass)
+
+  val RootResponse = """
 ...
 </style>
 <script type="text/javascript">
@@ -12,7 +15,8 @@ wlansd = new Array();
 wlansd[0]="/DCIM/,100OLYMP,0,0,0,0";
 ...
 """
-  val folderResponse = """
+
+  val FolderResponse = """
 ...
 </style>
 <script type="text/javascript">
@@ -21,19 +25,26 @@ wlansd[0]="/DCIM/100OLYMP/,OR.ORF,15441739,0,18229,43541";
 wlansd[0]="/DCIM/100OLYMP/,VI.AVI,95441739,0,18229,43541";
 ...
                      """
-  val fileContentResponse = "=== PHOTO SAMPLE ==="
+
+  val FileContentResponse = "=== PHOTO SAMPLE ==="
+  //val fileThumbnailResponse = this.getClass.getResource("/org/mauritania/photosync/100OLYMP/thumbnails/OR.ORF").getFile.getBytes().mkString
+  val HttpOk = 200
 
   def handle(t: HttpExchange) {
-    t.getRequestURI.toString match {
-      case "/DCIM" => sendRootResponse(t, rootResponse)
-      case "/DCIM/100OLYMP" => sendRootResponse(t, folderResponse)
-      case "/DCIM/100OLYMP/OR.ORF" => sendRootResponse(t, fileContentResponse)
+    val uri = t.getRequestURI.toString
+    logger.debug(s"Requested: $uri")
+    uri match {
+      case "/DCIM" => sendRootResponse(t, RootResponse)
+      case "/DCIM/100OLYMP" => sendRootResponse(t, FolderResponse)
+      case "/DCIM/100OLYMP/OR.ORF" => sendRootResponse(t, FileContentResponse)
+      //case "/get_thumbnail.cgi?DIR=100OLYMP/OR.ORF" => sendRootResponse(t, fileThumbnailResponse)
       case uri => sendRootResponse(t, s"Unexpected url: $uri")
     }
   }
 
   private def sendRootResponse(t: HttpExchange, response: String) {
-    t.sendResponseHeaders(200, response.length())
+    logger.debug(s"Response: $response")
+    t.sendResponseHeaders(HttpOk, response.length())
     val os = t.getResponseBody
     os.write(response.getBytes)
     os.close()
